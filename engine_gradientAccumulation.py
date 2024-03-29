d4c0f8a22ce98f2186b37e85e26896df241a4ed3
ZackLoken <zack@savannainstitute.org>
2024-03-28 20:49:48 -0500

    Update engine_gradientAccumulation.py

--- engine_gradientAccumulation.py
+++ engine_gradientAccumulation.py
@@ -29,13 +29,13 @@ def train_one_epoch(model, optimizer, data_loader, accumulation_steps, device, e
 
         loss_dict = model(images, targets)
 
-        losses = sum(loss for loss in loss_dict.values())
+        losses = sum(loss for loss in loss_dict.values()) 
 
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
-        losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+        losses_reduced = sum(loss for loss in loss_dict_reduced.values()) 
 
-        loss_value = losses_reduced.item() / accumulation_steps
+        loss_value = losses_reduced.item() 
 
         if not math.isfinite(loss_value):
             print("Loss is {}, stopping training".format(loss_value))
@@ -46,17 +46,22 @@ def train_one_epoch(model, optimizer, data_loader, accumulation_steps, device, e
 
         # accumulate gradients for specified number of batches (accumulation steps) before zeroing
         if ((batch_index + 1) % accumulation_steps == 0) or ((batch_index + 1) == len(data_loader)):
-            optimizer.zero_grad()
             optimizer.step()
+            optimizer.zero_grad()
+            
 
-        if lr_scheduler is not None:
-            lr_scheduler.step()
+            if lr_scheduler is not None:
+                lr_scheduler.step()
 
         metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
         metric_logger.update(lr=optimizer.param_groups[0]["lr"])
 
     return metric_logger
 
+
+# Modify the evaluate function to work with the gradient accumulation
+# The evaluate function is used to evaluate the model on the validation set
+# The function returns the coco_evaluator object which contains the evaluation results
 
 def _get_iou_types(model):
     model_without_ddp = model

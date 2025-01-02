c0819ced763e89f3c205baf75ad989ab30849d9d
Zack Loken <zack@savannainstitute.org>
2025-01-02 13:25:02 -0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -207,13 +207,14 @@ def collate_fn(batch):
     return tuple(zip(*batch))
 
 
-def warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor, accumulation_steps):
-    adjusted_warmup_iters = warmup_iters // accumulation_steps
+def warmup_lr_scheduler(optimizer, warmup_iters, warmup_factor):
+
     def f(x):
-        if x >= adjusted_warmup_iters:
+        if x >= warmup_iters:
             return 1
-        alpha = float(x) / adjusted_warmup_iters
+        alpha = float(x) / warmup_iters
         return warmup_factor * (1 - alpha) + alpha
+
     return torch.optim.lr_scheduler.LambdaLR(optimizer, f)
 
 
